063047996e1e0e7a69031aa9315424fdc2676f43
datatong <w.anthonytong@gmail.com>
2023-05-22 20:33:16 -0700

    console logour needed weather data from the json object

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ let searchInput = document.querySelector('#search-input');
 let searchBtn = document.querySelector('#search-button');
 
 //show a default location weather or placeholder
+
+//fetch
 async function fetchWeather(location) {
     const response = await fetch(
         `http://api.weatherapi.com/v1/current.json?key=06291ad7191745fa992190804230705&q=${location}`,
@@ -11,19 +13,30 @@ async function fetchWeather(location) {
             mode: 'cors',
         }
     );
-    const weatherData = await response.json()    
-    console.log(weatherData);
+    //add error handling here
+
+    const weatherObj = await response.json();    
+    //console.log(weatherObj);
+    (function logWeatherData() {
+        console.log(weatherObj.location.name);
+        console.log(weatherObj.location.region);
+        console.log(weatherObj.current.condition.text);
+        console.log(weatherObj.current.condition.icon);
+        console.log(weatherObj.current.temp_f);
+    })();
+
+    //show a loading message until fetch is complete (test with timeout)
 }
 
-//fetch weather data via search input
+//fetch weather data via submit form event
 searchForm.addEventListener('submit', (evt) => {
         evt.preventDefault();
-        getWeather();
+        searchWeather();
     });
 
-function getWeather() {
+//function to search input location
+function searchWeather() {
     let searchInput = document.querySelector('#search-input');
     const searchLocation = searchInput.value;
     fetchWeather(searchLocation);
 }
-//show a loading message until fetch is complete
